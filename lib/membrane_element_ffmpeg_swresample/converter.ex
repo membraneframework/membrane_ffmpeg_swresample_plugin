7edcef41f383cf995240cc42c5e7a8723268518f
Mateusz Front <mateusz.front@swmansion.com>
2017-04-26 18:46:49 +0200

    sink caps can now be supplied directly in options

--- lib/membrane_element_ffmpeg_swresample/converter.ex
+++ lib/membrane_element_ffmpeg_swresample/converter.ex
@@ -1,5 +1,6 @@
 defmodule Membrane.Element.FFmpeg.SWResample.ConverterOptions do
   defstruct \
+    sink_caps: Nil,
     source_caps: %Membrane.Caps.Audio.Raw{}
 end
 
@@ -29,22 +30,33 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
     ]}
   }
 
-  def handle_init %ConverterOptions{source_caps: source_caps} do
-    {:ok, %{source_caps: source_caps, native: nil}}
+  def handle_init %ConverterOptions{sink_caps: sink_caps, source_caps: source_caps} do
+    {:ok, %{sink_caps: sink_caps, source_caps: source_caps, native: nil}}
   end
 
-  def handle_caps(
-    :sink,
-    %Caps{format: sink_format, sample_rate: sink_rate, channels: sink_channels},
-    %{source_caps: %Caps{format: src_format, sample_rate: src_rate, channels: src_channels} = src_caps} = state
-  ) do
+  defp handle_all_caps_supplied(
+    %Caps{format: sink_format, sample_rate: sink_rate, channels: sink_channels} = _sink_caps,
+    %Caps{format: src_format, sample_rate: src_rate, channels: src_channels} = src_caps,
+    state
+  )do
     case ConverterNative.create(
       sink_format |> Caps.SerializedFormat.from_atom, sink_rate, sink_channels,
       src_format |> Caps.SerializedFormat.from_atom, src_rate, src_channels
-    ) do
+    )do
       {:ok, native} -> {:ok, [{:caps, {:source, src_caps}}], %{state | native: native}}
       {:error, reason} -> {:error, reason, state}
     end
+  end
+
+  def handle_prepare :stopped, %{sink_caps: %Caps{} = sink_caps, source_caps: source_caps} = state do
+    handle_all_caps_supplied sink_caps, source_caps, state
+  end
+  def handle_prepare _, state do
+    {:ok, state}
+  end
+
+  def handle_caps :sink, sink_caps, %{source_caps: source_caps} = state do
+    handle_all_caps_supplied sink_caps, source_caps, state
   end
 
   def handle_buffer :sink, _caps, %Membrane.Buffer{}, %{native: nil} do
