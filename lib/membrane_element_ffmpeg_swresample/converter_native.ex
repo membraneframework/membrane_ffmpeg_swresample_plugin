f3db86a8d9588b7ddf2108d57914212647e74824
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-04-09 14:22:11 +0200

    Tiny typespec fix

--- lib/membrane_element_ffmpeg_swresample/converter_native.ex
+++ lib/membrane_element_ffmpeg_swresample/converter_native.ex
@@ -8,6 +8,8 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter.Native do
     Bundlex.Loader.load_lib_nif!(:membrane_element_ffmpeg_swresample, :membrane_element_ffmpeg_swresample_converter)
   end
 
+  @opaque handle_t :: reference()
+
   @doc """
   Function creating native handler of converter.
 
@@ -18,7 +20,7 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter.Native do
   Returns {:ok, native_handle} or {:error, reason}.
   """
   @spec create(integer, integer, integer, integer, integer, integer) ::
-  {:ok, any} | {:error, any}
+  {:ok, handle_t} | {:error, any}
   def create(_sink_format, _sink_rate, _sink_channels, _src_format, _src_rate, _src_channels), do: raise "NIF fail"
 
   @doc """
@@ -31,7 +33,7 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter.Native do
   Note: Not all samples are guaranteed to be converted. Some of them may be stored
   in handle to be converted when long enough chunk is collected.
   """
-  @spec convert(any, binary) ::
+  @spec convert(handle_t, binary) ::
   {:ok, binary} | {:error, any}
   def convert(_native, _data), do: raise "NIF fail"
 
