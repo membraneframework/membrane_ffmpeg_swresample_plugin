cc74200bc370834f6e919d0ef525bd28f0c34c9b
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-04-09 14:05:35 +0200

    Replace Converter.Options module with def_options

--- lib/membrane_element_ffmpeg_swresample/converter.ex
+++ lib/membrane_element_ffmpeg_swresample/converter.ex
@@ -5,32 +5,35 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
   """
   use Membrane.Element.Base.Filter
   alias Membrane.Caps.Audio.Raw, as: Caps
-  alias __MODULE__.{Options, Native}
+  alias __MODULE__.Native
   alias Membrane.Buffer
+  alias Membrane.Caps.Matcher
   use Membrane.Helper
 
-  # @supported_caps [
-  #   %Caps{format: :f64le, channels: 1},
-  #   %Caps{format: :f32le, channels: 1},
-  #   %Caps{format: :s32le, channels: 1},
-  #   %Caps{format: :s16le, channels: 1},
-  #   %Caps{format: :u8,    channels: 1},
-  #   %Caps{format: :f64le, channels: 2},
-  #   %Caps{format: :f32le, channels: 2},
-  #   %Caps{format: :s32le, channels: 2},
-  #   %Caps{format: :s16le, channels: 2},
-  #   %Caps{format: :u8,    channels: 2},
-  # ]
+  @supported_caps {Caps, format: Matcher.one_of([:u8, :s16le, :s32le, :f32le, :f64le]), channels: Matcher.one_of([1, 2])}
 
-  def_known_source_pads %{
-    :source => {:always, :pull, :any}
-  }
+  def_known_source_pads source: {:always, :pull, @supported_caps}
 
-  def_known_sink_pads %{
-    :sink => {:always, {:pull, demand_in: :bytes}, :any},
-  }
+  def_known_sink_pads sink: {:always, {:pull, demand_in: :bytes}, @supported_caps}
 
-  def handle_init(%Options{sink_caps: sink_caps, source_caps: source_caps}) do
+  def_options sink_caps: [
+                type: :caps,
+                spec: Caps.t | nil,
+                default: nil,
+                description: """
+                Audio caps for sink pad (input). If set to nil (default value),
+                caps are assumed to be received from :sink
+                """
+              ],
+              source_caps: [
+                type: :caps,
+                description: """
+                Audio caps for souce pad (output)
+                """
+              ]
+
+  @impl true
+  def handle_init(%__MODULE__{sink_caps: sink_caps, source_caps: source_caps}) do
     {:ok, %{
       sink_caps: sink_caps,
       source_caps: source_caps,
@@ -39,6 +42,7 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
     }}
   end
 
+  @impl true
   def handle_prepare(:stopped, %{sink_caps: nil} = state), do:
     {:ok, state}
 
@@ -54,6 +58,7 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
   def handle_prepare(playback, state), do:
     super(playback, state)
 
+  @impl true
   def handle_caps(:sink, caps, _, state) do
     with {:ok, native} <- mk_native(caps, state.source_caps)
     do
@@ -63,19 +68,20 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
     end
   end
 
+  @impl true
   def handle_demand(:source, _size, :bytes, _, %{native: nil} = state), do:
     {:ok, state}
 
   def handle_demand(:source, size, :bytes, _, state), do:
     {{:ok, demand: {:sink, size}}, state}
 
+  @impl true
   def handle_process1(
     :sink,
     %Buffer{payload: payload},
     %{caps: caps},
     %{native: native, queue: q} = state
   ) do
-    # payload = buffers |> Enum.map(& &1.payload) |> IO.iodata_to_binary
     frame_size = (caps |> Caps.sample_size) * caps.channels
     with {:ok, {result, q}} when byte_size(result) > 0
       <- convert(native, frame_size, payload, q)
@@ -90,7 +96,7 @@ defmodule Membrane.Element.FFmpeg.SWResample.Converter do
   defp mk_native(
     %Caps{format: sink_format, sample_rate: sink_rate, channels: sink_channels},
     %Caps{format: src_format, sample_rate: src_rate, channels: src_channels}
-  )do
+  ) do
     Native.create(
       sink_format |> Caps.Format.serialize, sink_rate, sink_channels,
       src_format |> Caps.Format.serialize, src_rate, src_channels
