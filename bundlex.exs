680da452e69c4515cb86af2e04e50db406c12fc4
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-05-10 11:21:42 +0200

    [Bundlex] Don't add includes field on unix platform

--- bundlex.exs
+++ bundlex.exs
@@ -4,31 +4,37 @@ defmodule Membrane.Element.FFmpeg.SWResample.BundlexProject do
   def project do
     [
       nifs: [
-        membrane_element_ffmpeg_swresample_converter: [
-          sources: ["converter.c", "converter_lib.c"],
-          deps: [membrane_common_c: :membrane],
-          includes: ["ext/include"],
-          libs: libs(Bundlex.platform())
-        ]
+        membrane_element_ffmpeg_swresample_converter:
+          [
+            sources: ["converter.c", "converter_lib.c"],
+            deps: [membrane_common_c: :membrane]
+          ]
+          ++ platform_specific(Bundlex.platform())
       ]
     ]
   end
 
-  defp libs(:windows32) do
+  defp platform_specific(:windows32) do
     [
-      "ext/windows/32/avutil.lib",
-      "ext/windows/32/swresample.lib"
+      includes: ["ext/include"],
+      libs: [
+        "ext/windows/32/avutil.lib",
+        "ext/windows/32/swresample.lib"
+      ]
     ]
   end
 
-  defp libs(:windows64) do
+  defp platform_specific(:windows64) do
     [
-      "ext/windows/64/avutil.lib",
-      "ext/windows/64/swresample.lib"
+      includes: ["ext/include"],
+      libs: [
+        "ext/windows/64/avutil.lib",
+        "ext/windows/64/swresample.lib"
+      ]
     ]
   end
 
-  defp libs(_) do
-    ["avutil", "swresample"]
+  defp platform_specific(_) do
+    [libs: ["avutil", "swresample"]]
   end
 end
