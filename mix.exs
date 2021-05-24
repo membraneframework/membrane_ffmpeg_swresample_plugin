e80827f0360b9fc590c1bc6a05557cce1e72d15b
Mateusz Front <mateusz.front@swmansion.com>
2021-05-24 12:06:14 +0200

    bump to 0.6.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Membrane.FFmpeg.SWResample.Mixfile do
   use Mix.Project
 
   @github_url "https://github.com/membraneframework/membrane_ffmpeg_swresample_plugin"
-  @version "0.5.0"
+  @version "0.6.0"
 
   def project do
     [
@@ -66,8 +66,8 @@ defmodule Membrane.FFmpeg.SWResample.Mixfile do
       {:membrane_core, "~> 0.6.0"},
       {:membrane_caps_audio_raw, "~> 0.3.0"},
       {:bunch, "~> 1.3.0"},
-      {:unifex, "~> 0.3.2"},
-      {:membrane_common_c, "~> 0.5.0"},
+      {:unifex, "~> 0.4.0"},
+      {:membrane_common_c, "~> 0.6.0"},
       {:bundlex, "~> 0.4.0"},
       {:mockery, "~> 2.1", runtime: false}
     ]
