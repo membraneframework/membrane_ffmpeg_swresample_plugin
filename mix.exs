a5e402bcd534523d6c82eef3375fbe0deaf0ee9a
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-03-18 12:11:26 +0100

    Bump version to 0.12.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Membrane.FFmpeg.SWResample.Mixfile do
   use Mix.Project
 
   @github_url "https://github.com/membraneframework/membrane_ffmpeg_swresample_plugin"
-  @version "0.11.0"
+  @version "0.12.0"
 
   def project do
     [
