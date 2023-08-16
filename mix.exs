8bb517ed3147264fa046844e44a3d82c98d37e2d
Mateusz Front <mateusz.front@swmansion.com>
2023-08-16 11:46:52 +0200

    bump version to 0.17.3

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Membrane.FFmpeg.SWResample.Mixfile do
   use Mix.Project
 
   @github_url "https://github.com/membraneframework/membrane_ffmpeg_swresample_plugin"
-  @version "0.17.2"
+  @version "0.17.3"
 
   def project do
     [
