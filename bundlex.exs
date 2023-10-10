af0102ac77d10088158e073b73a16d68bbc1c9f9
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-10-10 11:34:53 +0200

    Unify get_ffmpeg_url

--- bundlex.exs
+++ bundlex.exs
@@ -1,19 +1,20 @@
 defmodule Membrane.FFmpeg.SWResample.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg() do
+  defp get_ffmpeg_url() do
+    membrane_precompiled_url_prefix =
+      "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
+
     case Bundlex.get_target() do
       %{os: "linux"} ->
         {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/latest/ffmpeg_linux.tar.gz"}
+         "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"}
 
       %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/latest/ffmpeg_macos_intel.tar.gz"}
+        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"}
 
       %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/latest/ffmpeg_macos_arm.tar.gz"}
+        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"}
 
       _other ->
         nil
@@ -32,7 +33,7 @@ defmodule Membrane.FFmpeg.SWResample.BundlexProject do
         interface: :nif,
         sources: ["converter.c", "converter_lib.c"],
         deps: [membrane_common_c: :membrane, unifex: :unifex],
-        os_deps: [{[get_ffmpeg(), :pkg_config], ["libswresample"]}],
+        os_deps: [{[get_ffmpeg_url(), :pkg_config], ["libswresample", "libavutil"]}],
         preprocessor: Unifex
       ]
     ]
