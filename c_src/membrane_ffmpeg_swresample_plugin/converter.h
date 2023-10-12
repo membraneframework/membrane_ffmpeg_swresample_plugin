ed7e4f24198b30541abd6facc452c7c29a44e62e
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-10-12 12:06:00 +0200

    Remove unecessary warning ignoring

--- c_src/membrane_ffmpeg_swresample_plugin/converter.h
+++ c_src/membrane_ffmpeg_swresample_plugin/converter.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wall"
-#pragma GCC diagnostic ignored "-Wextra"
 #include <membrane/membrane.h>
-#pragma GCC diagnostic pop
 
 #include <erl_nif.h>
 #include <stdio.h>
