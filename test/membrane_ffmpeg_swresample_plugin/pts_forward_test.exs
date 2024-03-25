cd2486d43eee3c7bbfe28406f3192f8821367719
Bartek Chaliński <b.chalinski@gmail.com>
2024-03-25 19:35:11 +0100

    pts test update

--- test/membrane_ffmpeg_swresample_plugin/pts_forward_test.exs
+++ test/membrane_ffmpeg_swresample_plugin/pts_forward_test.exs
@@ -9,12 +9,10 @@ defmodule Membrane.FFmpeg.SWResample.PtsForwardTest do
 
   test "pts forward test" do
     input_stream_format = %RawAudio{sample_format: :s16le, sample_rate: 16_000, channels: 2}
-    # input_stream_format = %RawAudio{sample_format: :u8, sample_rate: 8_000, channels: 1}
     output_stream_format = %RawAudio{sample_format: :s32le, sample_rate: 32_000, channels: 2}
 
     # 32 frames * 2048 bytes
     fixture_path = Path.expand(Path.join(__DIR__, "/../fixtures/input_s16le_stereo_16khz.raw"))
-    # fixture_path = Path.expand(Path.join(__DIR__, "/../fixtures/input_u8_mono_8khz.raw"))
 
     spec = [
       child(:source, %Membrane.Testing.Source{output: buffers_from_file(fixture_path)})
@@ -27,11 +25,12 @@ defmodule Membrane.FFmpeg.SWResample.PtsForwardTest do
 
     pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
     assert_start_of_stream(pipeline, :sink)
-
-    # Enum.each(0..31, fn index ->
-    #   assert_sink_buffer(pipeline, :sink, %Membrane.Buffer{pts: out_pts})
-    #   assert out_pts == index * 31250000
-    # end)
+    assert_sink_buffer(pipeline, :sink, _buffer)
+    Enum.each(0..30, fn index ->
+      assert_sink_buffer(pipeline, :sink, %Membrane.Buffer{pts: out_pts})
+      assert out_pts == index * 31250000
+    end)
+    assert_sink_buffer(pipeline, :sink, %Membrane.Buffer{pts: 937500000})
 
     assert_end_of_stream(pipeline, :sink)
     Testing.Pipeline.terminate(pipeline)
@@ -54,10 +53,10 @@ defmodule Membrane.FFmpeg.SWResample.PtsForwardTest do
   def split_binary(binary, acc \\ [])
 
   def split_binary(<<binary::binary-size(2048), rest::binary>>, acc) do
-    split_binary(rest, acc ++ [binary])
+    split_binary(rest, [binary] ++ acc)
   end
 
   def split_binary(rest, acc) when byte_size(rest) <= 2048 do
-    Enum.concat(acc, [rest])
+    Enum.reverse(acc) ++ [rest]
   end
 end
