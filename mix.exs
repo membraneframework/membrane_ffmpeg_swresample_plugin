f0df5622ac44a4e83e0482a8544a2a8f5e7cd362
Daniel Morandini <danielmorandini@me.com>
2025-11-24 18:33:45 +0100

    Update deps

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Membrane.FFmpeg.SWResample.Mixfile do
       {:unifex, "~> 1.1"},
       {:membrane_common_c, "~> 0.16.0"},
       {:bundlex, "~> 1.2"},
-      {:membrane_precompiled_dependency_provider, "~> 0.1.0"},
+      {:membrane_precompiled_dependency_provider, "~> 0.2.1"},
       # Testing
       {:mockery, "~> 2.1", runtime: false},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
